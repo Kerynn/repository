b6c8dc5991c9b9f32be1cb287ad577a477b77f2d
Kerynn Davis <kerynn.davis@gmail.com>
2022-09-03 11:12:24 -0700

    Add email problem

--- friends.rb
+++ friends.rb
@@ -1,13 +1,32 @@
-numbers = [13, 80, 26, 48, 53, 37, 98, 2]
+# numbers = [13, 80, 26, 48, 53, 37, 98, 2]
+#
+# numbers.each do |number|
+#   if number > 30
+#   p number
+# end
+# end
+#
+# numbers.each do |number|
+#   if number < 60
+#     p number
+#   end
+# end
 
-numbers.each do |number|
-  if number > 30
-  p number
-end
-end
 
-numbers.each do |number|
-  if number < 60
-    p number
-  end
-end
+email_contents = {
+  "sender" => "Aly Kessner",
+  "subject_title" => "Meeting Confirmation",
+  "body" => "Just wanted to make sure we are meeting at 3",
+  "closing" => "Please let me know",
+  "signature" => "Aly Kessner, aly@gmail.com 12334455"
+}
+
+p email_contents
+
+
+puts "From: #{email_contents["sender"]}
+Subject: #{email_contents["subject_title"]}
+Hello Anhni,
+#{email_contents["body"]}
+#{email_contents["closing"]}
+#{email_contents["signature"]}"
