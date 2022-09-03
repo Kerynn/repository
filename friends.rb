e21c72d7b17e30655c66940ec6596817178d3dd9
Kerynn Davis <kerynn.davis@gmail.com>
2022-09-03 10:37:53 -0700

    Add friends file

---
+++ friends.rb
@@ -0,0 +1,13 @@
+numbers = [13, 80, 26, 48, 53, 37, 98, 2]
+
+numbers.each do |number|
+  if number > 30
+  p number
+end
+end
+
+numbers.each do |number|
+  if number < 60
+    p number
+  end
+end
