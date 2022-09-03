6657c1078c1c4be3e8abc36a59c6fa4ca56533b6
Kerynn Davis <kerynn.davis@gmail.com>
2022-09-03 10:09:22 -0700

    Add elsif

--- help.rb
+++ help.rb
@@ -7,9 +7,12 @@
 # p friends.unshift("Tim")
 
 friend = "Frank"
+friend2 = "Sam"
 
 if friend == "Nicole"
   puts "Hi #{friend}!"
+elsif friend2 == "Noona"
+  puts "You are so cool!"
 else
   puts "Goodbye!"
 end
