4b99f221c6e51bfc383fdda59bd96ad54d68dacb
Kerynn Davis <kerynn.davis@gmail.com>
2022-09-03 10:21:54 -0700

    Add hashes

--- help.rb
+++ help.rb
@@ -6,13 +6,32 @@
 #
 # p friends.unshift("Tim")
 
-friend = "Frank"
-friend2 = "Sam"
+# friend = "Frank"
+# friend2 = "Sam"
+#
+# if friend == "Nicole"
+#   puts "Hi #{friend}!"
+# elsif friend2 == "Noona"
+#   puts "You are so cool!"
+# else
+#   puts "Goodbye!"
+# end
 
-if friend == "Nicole"
-  puts "Hi #{friend}!"
-elsif friend2 == "Noona"
-  puts "You are so cool!"
-else
-  puts "Goodbye!"
-end
+
+person = {
+  "name" => "Kerynn",
+  "hair" => "brown",
+  "age" => 34,
+  "alive" => true
+}
+
+p person
+p person["name"]
+p person["age"]
+
+person["has_pet"] = true
+person["last_name"] = "Davis"
+
+p person
+p person.keys
+p person.values
